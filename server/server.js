30239c5fb3fc4d676c233f6d5529d039ce9d0413
idmacleod <idmacleod@gmail.com>
2020-05-19 15:26:53 +0100

    Installing pody-paser on server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-
+const parser = require('body-parser');
 const cors = require('cors');
+app.use(parser.json());
 app.use(cors());
 
 const MongoClient = require('mongodb').MongoClient;
